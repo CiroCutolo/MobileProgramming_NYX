9b3480bf59fad14168556a859688bfa86ef4daba
Gaetano Frasca <gaetanofra02@gmail.com>
2024-06-04 23:31:18 +0200

    Update AccountScreen.jsx

--- navigation/screens/AccountScreen.jsx
+++ navigation/screens/AccountScreen.jsx
@@ -3,11 +3,9 @@ import { View, Text } from 'react-native';
 import AccountList from '../../Components/AccountList.tsx';
 
 export default function AccountScreen({ navigation, route }) {
-    const { utente } = route.params;
-
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
-            <AccountList utente={utente} />
+            <AccountList />
         </View>
     );
 }
